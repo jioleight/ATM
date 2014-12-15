ebbd6106d9647fa9e442c74cf562466b10479aee
jioleight <jiofreed01@gmail.com>
2014-12-15 23:06:22 +0800

    Access UI
    initial ui for access ui

--- ATM.C
+++ ATM.C
@@ -2,7 +2,25 @@
 #include <conio.h>
 #include <stdio.h>
 
+accview();
+
 main()
 {
-
+	int ch;
+	clrscr();
+	
+	accview();
+	
+}
+accview()
+{
+	gotoxy(30, 9);
+	printf("===== USC BANKING SERVICE =====");
+	gotoxy(30, 10);
+	printf("Input Account No.: ");
+	gotoxy(30, 11);
+	printf("Input Access Code: ");
+	gotoxy(30, 12);
+	printf("===============================");
+	
 }
