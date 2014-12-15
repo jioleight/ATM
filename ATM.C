b96c1ee20706f0aeb9a11f4a12e7da385de692fe
jioleight <jiofreed01@gmail.com>
2014-12-16 00:15:49 +0800

    access ui
    added trap for length of acc and password. if less or more = invalid

--- ATM.C
+++ ATM.C
@@ -3,24 +3,58 @@
 #include <stdio.h>
 
 accview();
+int verify(char *acc, char *password);
 
 main()
 {
-	int ch;
+	int choice, res;
+	char *acc[8], *password[4];
 	clrscr();
 	
 	accview();
 	
+	gotoxy(43, 10);
+	gets(&acc);
+	
+	gotoxy(43, 11);
+	gets(&password);
+	
+	res = verify(acc, password);
+	
+	if(res == 1)
+	{
+		gotoxy(23, 15);
+		printf("Does not meet requirements");
+		sleep(2);
+		main();
+	}
+	else
+	{
+	
+	}
+	getch();
+	return 0;
 }
 accview()
 {
-	gotoxy(30, 9);
+	gotoxy(24, 9);
 	printf("===== USC BANKING SERVICE =====");
-	gotoxy(30, 10);
+	gotoxy(24, 10);
 	printf("Input Account No.: ");
-	gotoxy(30, 11);
+	gotoxy(24, 11);
 	printf("Input Access Code: ");
-	gotoxy(30, 12);
+	gotoxy(24, 12);
 	printf("===============================");
 	
+}
+int verify(char *acc, char *password)
+{
+	if(strlen(acc) != 8 || strlen(password) != 4)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
