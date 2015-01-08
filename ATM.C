86358b2d8de6ef3c99a659d57616eb9616e05d5c
jioleight <jiofreed01@gmail.com>
2015-01-09 02:32:14 +0800

    struct or file?
    what to use for login?

--- ATM.C
+++ ATM.C
@@ -2,14 +2,22 @@
 #include <conio.h>
 #include <stdio.h>
 
+struct userdb{
+	char accdb;
+	int passdb; 
+};
 accview();
-int verify(char *acc, char *password);
-
-main()
-{
+menu();
+int req(char *acc, int *password);
+main(){
+	struct userdb udb1, udb2, udb3,udb4,udb5;
 	int choice, res;
-	char *acc[8], *password[4];
+	char *acc[8];
+	int *password[4];
 	clrscr();
+	
+	strcpy(udb1.accdb, "09303470");
+	strcpy(udb1.passdb, "1234");
 	
 	accview();
 	
@@ -19,7 +27,7 @@ main()
 	gotoxy(43, 11);
 	gets(&password);
 	
-	res = verify(acc, password);
+	res = req(acc, password);
 	
 	if(res == 1)
 	{
@@ -28,9 +36,12 @@ main()
 		sleep(2);
 		main();
 	}
-	else
+	else if(res == 0)/*login*/
 	{
-	
+		if(strcmp(udb1.accdb, acc) == 1)
+		{
+			puts("success");
+		}
 	}
 	getch();
 	return 0;
@@ -47,7 +58,10 @@ accview()
 	printf("===============================");
 	
 }
-int verify(char *acc, char *password)
+menu()
+{
+}
+int req(char *acc, int *password)
 {
 	if(strlen(acc) != 8 || strlen(password) != 4)
 	{
