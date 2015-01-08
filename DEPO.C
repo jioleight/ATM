d6ab4adaa6ff1001378948280c3635a8d3cf67d7
ptpyn <paul_tapayan@yahoo.com>
2015-01-09 00:49:59 +0800

    Revert "depo revised"
    This reverts commit 287197422cf680f20ace09d2894dee8072853157.

--- DEPO.C
+++ DEPO.C
@@ -1,24 +1,32 @@
+/*******************************
+Name: Paul Albert Tapayan
+Project: ATM
+Date: 1/1/2015
+********************************/
+
+
 #include "ATM.H"
 #include <stdio.h>
 #include <conio.h>
 
 float deposit(char *acc)
 {
- auto int acc;
+ auto int acc_no;
    float add_money;
 
    printf("Enter account number you want to deposit money:");
-   scanf("%d",&acc);
+   scanf("%d",&acc_no);
    printf("\nThe current balance for account %d is %f \n",
-   acc, acc.available_balance);
+   acc_no, account[acc_no-1].available_balance);
    printf("\nEnter money you want to deposit :  ");
    scanf("%f",&add_money);
 
-   while (acc=acc.acc_number)
+   while (acc_no=account[acc_no-1].acc_number)
    {
-         acc.available_balance = acc.available_balance+add_money;
+         account[acc_no-1].available_balance=
+         account[acc_no-1].available_balance+add_money;
          printf("\nThe New balance for account %d is %f \n",
-         acc, acc.available_balance);
+         acc_no, account[acc_no-1].available_balance);
          break;
-   }acc++;
+   }acc_no++;
 }
