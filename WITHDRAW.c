73d1c5ad8cac0b32094d8bd30cec4b4cb8db4970
burceficationprogrammer <watashiwaree@yahoo.com>
2015-01-09 19:59:35 +0800

    REAL withdraw.c
    mao ni sya ang tinuod na withdraw. dummy rana ang "manage.txt" na database kay wa pako kabalo sa ngalan sa ato database...
    
    P.S. wa pa sad ni nako gi loop. gi suwayan nako, di sya mu gawas ambot lang ngano
    
    **** ingna lang nya ko if naa mo ma bag.o salamat!!!!

---
+++ WITHDRAW.c
@@ -0,0 +1,64 @@
+#include"ATM.H"
+#include<stdio.h>
+#include <stdlib.h>
+#include<dos.h>
+
+float withdraw(char *acc)
+{
+	float wd, bal;
+	int day, mon, yr, hr, min, sec;
+	char *name;
+	FILE *f;
+	struct time t;
+	struct date d;
+
+	f=fopen("manage.txt","a"); 
+	gettime(&t);
+	getdate(&d);
+	fprintf(f,"%02d / %02d / %04d\n",d.da_mon,d.da_day,d.da_year);
+	fprintf(f,"%02d : %02d : %02d\n",t.ti_hour,t.ti_min,t.ti_sec);
+	fprintf(f,"%s \n",acc);
+	fprintf(f,"%s \n",name);
+	fprintf(f,"%.2f \n",bal);
+	fclose(f);
+	
+	f=fopen("manage.txt","r");
+	fscanf(f,"%d / %d / %d\n",&mon,&day,&yr);
+	fscanf(f,"%d : %d : %d\n",&hr,&min,&sec);	
+	fscanf(f,"%s \n",acc);
+	fscanf(f,"%[^\n]\ns \n",name);
+	fscanf(f,"%f \n",&bal);
+	fclose(f);
+
+	clrscr();
+
+	printf("=====WITHDRAWAL=====");
+	printf("\nDate: %d / %d / %d",mon,day,yr);
+	printf("\nTime: %d / %d / %d",hr,min,sec);
+	printf("\nName: %s",name);
+	printf("\nAccount Number: %s",acc);
+	printf("\nCurrent Balance: %.2f",bal);
+	printf("\nWithdrawal Amount: Php ");
+	scanf("%f",&wd);
+	if(bal>=wd)
+	{
+		bal=bal-wd;
+		printf("\n\nPhp %.2f was deducted",wd);
+	}
+	else
+	{
+		printf("\n\nInsufficient Funds!!! Press a key to continue....");
+	}
+	printf("\nRemaining Balance: Php %.2f",bal);
+	printf("\nAgain [y/n]? ");
+	
+	f=fopen("manage.txt","a");
+	gettime(&t);
+	getdate(&d);
+	fprintf(f,"%02d / %02d / %04d\n",d.da_mon,d.da_day,d.da_year);
+	fprintf(f,"%02d : %02d : %02d\n",t.ti_hour,t.ti_min,t.ti_sec);
+	fprintf(f,"%[^\n]\ns \n",name);
+	fprintf(f,"%s \n",acc);
+	fprintf(f,"%f \n",bal);
+	fclose(f);
+}
