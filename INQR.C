2e70aa4cc7a9bf826b8609cfe489d3560c29b46b
burceficationprogrammer <watashiwaree@yahoo.com>
2015-01-09 22:39:34 +0800

    updated

--- INQR.C
+++ INQR.C
@@ -1,8 +1,33 @@
-#include "ATM.H"
-#include <stdio.h>
-#include <conio.h>
+#include"ATM.H"
+#include<stdio.h>
+#include <stdlib.h>
+#include<dos.h>
 
 float inquire(char *acc)
 {
+	float bal;
+	int day, mon, yr, hr, min, sec;
+	char *name;
+	FILE *f;
+	struct time t;
+	struct date d;
+	
+	f=fopen("manage.txt","r");
+	fscanf(f,"%d / %d / %d\n",&mon,&day,&yr);
+	fscanf(f,"%d : %d : %d\n",&hr,&min,&sec);	
+	fscanf(f,"%s \n",acc);
+	fscanf(f,"%[^\n]\ns \n",name);
+	fscanf(f,"%f \n",&bal);
+	fclose(f);
 
-}
+	clrscr();
+
+	printf("=====BALANCE INQUIRY=====");
+	printf("\nDate: %d / %d / %d",mon,day,yr);
+	printf("\nTime: %d / %d / %d",hr,min,sec);
+	printf("\nName: %s",name);
+	printf("\nAccount Number: %s",acc);
+	printf("\nCurrent Balance: %.2f",bal);
+	printf("\nPress any key to return to main menu");
+	getch();
+}
